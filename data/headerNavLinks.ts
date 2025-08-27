26d44cac850014551229163bca1439efd9e4bbc1
✅ Daniel Danielecki <daniel.danielecki@mail.com>
2025-08-27 11:31:48 +0200

    feat: add new navigation links for About, Education, iOS App, and Shop

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,12 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/tags', title: 'Tags' },
+  { href: 'https://education.ditectrev.com', title: 'Education', external: true },
+  { href: 'https://apps.apple.com/app/cloudmaster-swift/id6503601139', title: 'iOS App', external: true },
+  { href: 'https://shop.ditectrev.com/', title: 'Shop', external: true },
+  // { href: '/projects', title: 'Projects' },
 ]
 
 export default headerNavLinks
